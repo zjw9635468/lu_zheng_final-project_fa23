2c1d6ef6885259a5e56dccfb338f5e5dd9cff12c
Geralt5 <865201794@qq.com>
2023-12-24 20:01:28 -0500

    aonther change to fronend

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, text
-db_connection_string = "mysql+pymysql://hx8hza0129ew5qo6qcx9:pscale_pw_shPNhH52KRX4io6iJaG79nIt7KIDbv9n2Erb9MEVnz@aws.connect.psdb.cloud/lu_zheng_database_project?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://w58g1hh6z47z1y9cdo7g:pscale_pw_KqWGRx5MJNauDGskh5ojUVuAWc6TZuJ2Zd7WqOwHmEv@aws.connect.psdb.cloud/lu_zheng_database_project?charset=utf8mb4"
 engine = create_engine(db_connection_string, connect_args = {
     "ssl": {
         "ssl_ca": "/etc/ssl/cert.pem"
